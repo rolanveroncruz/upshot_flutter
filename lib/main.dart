ec821073acf2ad0cf320650b77af428d62884076
RolanVC Upshot <86640957+rolan-upshot@users.noreply.github.com>
2023-01-20 07:24:30 +0800

    still working on login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import '../shared/sp_helper.dart';
+import '../shared/login.dart';
 import 'screens/login.dart';
 import 'screens/home.dart';
 void main() {
@@ -14,6 +15,16 @@ class UpshotApp extends StatefulWidget {
 }
 
 class _UpshotAppState extends State<UpshotApp> {
+   late UserLogin userlogin;
+   late SPHelper helper;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    helper.init();
+    super.initState();
+
+  }
   
   @override
   Widget build(BuildContext context) {
