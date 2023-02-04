f314f18c8b2eae21f15f11aa79540617b9a4d989
RolanVC Upshot <86640957+rolan-upshot@users.noreply.github.com>
2023-02-04 23:05:23 +0800

    added splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:upshot_flutter/data/directory.dart';
 import 'package:upshot_flutter/screens/activity.dart';
 import 'package:upshot_flutter/screens/insights.dart';
 import 'package:upshot_flutter/screens/profile.dart';
+import 'package:upshot_flutter/screens/splash.dart';
 import 'screens/login.dart';
 import 'screens/home.dart';
 
@@ -12,36 +13,25 @@ void main() {
   runApp(const UpshotApp());
 }
 
-class UpshotApp extends StatefulWidget {
+class UpshotApp extends StatelessWidget {
   const UpshotApp({super.key});
 
   @override
-  State<UpshotApp> createState() => _UpshotAppState();
-}
-
-class _UpshotAppState extends State<UpshotApp> {
-   bool isLoggedIn = true;
-   ServerDirectory serverDirectory = ServerDirectory();
-
-  @override
-  void initState() {
-    super.initState();
-    serverDirectory.init();
-  }
-
-    
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-      theme: ThemeData(primarySwatch: Colors.blue),
+     return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
       routes:{
         '/login':(context)=>Login(),
         '/home': (context)=>HomeScreen(),
         '/insights': (context)=>InsightsScreen(),
         '/activity': (context)=>ActivityScreen(),
         '/profile': (context)=>ProfileScreen()
-      }, initialRoute: '/login',
+      } 
     );
   }
 }
---
+++ lib/screens/splash.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:upshot_flutter/screens/login.dart';
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+     Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Login())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+  }
+}
